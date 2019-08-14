ff62ccfaee9e38c3190276e2e513d794a3310426
RiqCodes <keekman3@gmail.com>
2019-08-14 00:49:42 -0700

    updated button

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     var config = {
         apiKey: "AIzaSyBPCAxjycmuYaeY8RISjpPzUOSfmZIbQrQ",
-        authDomain: "train-scheduler-68643.firebaseapp.com",
+        authDomain: "train-scheduler-56269.firebaseapp.com",
         databaseURL: "https://train-scheduler-68643.firebaseio.com",
-        projectId: "train-scheduler-68643",
-        storageBucket: "train-scheduler-68643.appspot.com",
+        projectId: "train-scheduler-56269",
+        storageBucket: "train-scheduler-56269.appspot.com",
         messagingSenderId: "934455543386"
     };
     firebase.initializeApp(config);
